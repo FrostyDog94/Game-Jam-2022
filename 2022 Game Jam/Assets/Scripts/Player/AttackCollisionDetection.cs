c9185b7c0a892b893baff3d0b57f2a9ee6ea5e24
Andrew Soguero <andrewsoguero@gmail.com>
2022-05-21 19:25:52 -0600

    Enemies change to dead state when attacked

--- 2022 Game Jam/Assets/Scripts/Player/AttackCollisionDetection.cs
+++ 2022 Game Jam/Assets/Scripts/Player/AttackCollisionDetection.cs
@@ -10,7 +10,7 @@ public class AttackCollisionDetection : MonoBehaviour
     {
         if (other.tag == "Enemy")
         {
-            Destroy(other.gameObject);
+            StartCoroutine(other.GetComponent<Enemy>().State_Dead());
             
         }
     }
