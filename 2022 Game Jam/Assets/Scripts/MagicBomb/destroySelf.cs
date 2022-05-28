a697605f7d2e7d6273b5b2a0f02a1cfb90c50540
Andrew Soguero <andrewsoguero@gmail.com>
2022-05-27 18:46:14 -0600

    Magic grenade

---
+++ 2022 Game Jam/Assets/Scripts/MagicBomb/destroySelf.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroySelf : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(waitThenDie());
+    }
+
+
+
+    IEnumerator waitThenDie()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Destroy(this.gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy" && other.GetComponent<Enemy>().currentState == ENEMY_STATE.ALIVE)
+        {
+
+            other.GetComponent<Enemy>().TakeDamage(75);
+
+        }
+    }
+
+
+}
---
+++ 2022 Game Jam/Assets/Scripts/Player/MagicBomb.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicBomb : MonoBehaviour
+{
+    public GameObject magicBomb;
+    public Transform spawnPosition;
+    public float throwForce = 10.0f;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.Play("MagicBomb");
+        }
+    }
+
+    public void bomb()
+    {
+        GameObject newBomb = Instantiate(magicBomb, spawnPosition.position, transform.rotation);
+        newBomb.GetComponent<Rigidbody>().AddForce(transform.TransformDirection(Vector3.forward) * throwForce);
+    }
+}
